31dc941eceb9ae816d40865adfadc2f9eeec7b00
wangqi1115 <wangqi1115@jd.com>
2023-06-02 12:23:00 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import ElementUI from 'element-ui';
 
 import "./utils/axios";
 
-Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
